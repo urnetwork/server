90c2ce729a52b627e0c4a4d34a113679c4ff4542
Brien Colwell <xcolwell@gmail.com>
2024-01-02 01:58:39 -0800

    run-main notes

--- bringyourctl/run-main.sh
+++ bringyourctl/run-main.sh
@@ -2,6 +2,9 @@
 
 env=main
 
+# you must port forward to the main db and redis from 127.0.0.1
+# e.g. ssh -i ${main-data-key} -L 5432:127.0.0.1:5432 -L 6379:127.0.0.1:6379 by@${main-data-host} -N
+
 export WARP_VERSION="0.0.0-local"; \
 export WARP_ENV="$env"; \
 export BRINGYOUR_POSTGRES_HOSTNAME="127.0.0.1"; \
