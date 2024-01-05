3eda6dd3fc87c415c024a674a553517bb8701f93
Brien Colwell <xcolwell@gmail.com>
2024-01-05 01:59:43 -0800

    api updates

---
+++ api/handlers/provider_handlers.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+
+// FIXME stats_provider_model
+
+func StatsProvidersOverviewLast90(w http.ResponseWriter, req *http.Request) {
+}
+
+
+func StatsProviders(w http.ResponseWriter, req *http.Request) {
+}
+
+
+func StatsProviderLast90(w http.ResponseWriter, req *http.Request) {
+}
