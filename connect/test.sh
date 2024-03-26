9c8cb25f3fda8b1f93d7f553471685221fcff8d5
Brien Colwell <xcolwell@gmail.com>
2024-03-26 01:48:17 -0700

    update

--- connect/test.sh
+++ connect/test.sh
@@ -8,9 +8,8 @@ for d in `find . -iname '*_test.go' | xargs -n 1 dirname | sort | uniq | paste -
         export WARP_ENV="local"; \
             export BRINGYOUR_POSTGRES_HOSTNAME="local-pg.bringyour.com"; \
             export BRINGYOUR_REDIS_HOSTNAME="local-redis.bringyour.com"; \
-            GORACE="log_path=profile/race.out halt_on_error=1" go test -v -race -timeout 30m | grep --color=always -e "^" -e "$match"
-            # -cpuprofile profile/cpu -memprofile profile/memory -trace profile/trace
-            # -coverprofile profile/cover
+            GORACE="log_path=profile/race.out halt_on_error=1" go test -v -race -cpuprofile profile/cpu -memprofile profile/memory -timeout 30m | grep --color=always -e "^" -e "$match"
+            # -trace profile/trace -coverprofile profile/cover 
         if [[ ${PIPESTATUS[0]} != 0 ]]; then
             exit ${PIPESTATUS[0]}
         fi
@@ -20,4 +19,4 @@ done
 # stdbuf -i0 -o0 -e0 
 
 # go tool trace profile/trace
-# go tool pprof profile/cpu
+# PPROF_BINARY_PATH=. go tool pprof profile/cpu
