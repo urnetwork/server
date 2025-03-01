e8194d5cd8e901ec9811987a914e4bf1fa0ff3af
Brien Colwell <xcolwell@gmail.com>
2025-03-01 10:45:36 -0800

    bringyourctl use go run to always use the latest code

--- bringyourctl/run-local.sh
+++ bringyourctl/run-local.sh
@@ -4,4 +4,4 @@ export WARP_VERSION="0.0.0-local"; \
 export WARP_ENV="local"; \
 export BRINGYOUR_POSTGRES_HOSTNAME="local-pg.bringyour.com"; \
 export BRINGYOUR_REDIS_HOSTNAME="local-redis.bringyour.com"; \
-./bringyourctl "$@"
+go run main.go "$@"
--- bringyourctl/run-main.sh
+++ bringyourctl/run-main.sh
@@ -9,4 +9,4 @@ export WARP_VERSION="0.0.0-local"; \
 export WARP_ENV="$env"; \
 export BRINGYOUR_POSTGRES_HOSTNAME="127.0.0.1"; \
 export BRINGYOUR_REDIS_HOSTNAME="127.0.0.1"; \
-./bringyourctl "$@"
+go run main.go "$@"
