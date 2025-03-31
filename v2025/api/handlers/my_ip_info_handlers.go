2c95e830ed3326cfe0890c480f012361624e7650
Brien Colwell <xcolwell@gmail.com>
2025-03-31 14:27:41 -0700

    2025.3.31-58656426

---
+++ v2025/api/handlers/my_ip_info_handlers.go
@@ -0,0 +1,128 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/ipinfo/go/v2/ipinfo"
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/api/myipinfo/landmarks"
+	"github.com/urnetwork/server/api/myipinfo/myinfo"
+	"github.com/urnetwork/server/controller"
+	"github.com/urnetwork/server/model"
+)
+
+
+type response struct {
+	Info               myinfo.MyInfo              `json:"info"`
+	ExpectedRTTs       []landmarks.LandmarkAndRTT `json:"landmarks"`
+	ConnectedToNetwork bool                       `json:"connected_to_network"`
+}
+
+// matches the first group to the IPV6 address when the input is <ipv6>:<port>
+// example: 2001:5a8:4683:4e00:3a76:dcec:7cb:f180:40894
+var malformedIPV6WithPort = regexp.MustCompile(`^(.+):\d+$`)
+
+func MyIPInfo(w http.ResponseWriter, r *http.Request) {
+
+	remoteAddr := r.Header.Get("X-UR-Forwarded-For")
+
+	if remoteAddr == "" {
+		remoteAddr = r.Header.Get("X-Forwarded-For")
+	}
+
+	if remoteAddr == "" {
+		remoteAddr = r.RemoteAddr
+	}
+
+	columnCount := strings.Count(remoteAddr, ":")
+	bracketCount := strings.Count(remoteAddr, "[")
+
+	var addressOnly string
+
+	// if the address is malformed, extract the address from the address:port string
+	if columnCount > 1 && bracketCount == 0 {
+		groups := malformedIPV6WithPort.FindStringSubmatch(remoteAddr)
+
+		if len(groups) > 1 {
+			addressOnly = groups[1]
+		}
+
+	} else {
+		var err error
+		addressOnly, _, err = net.SplitHostPort(remoteAddr)
+		if err != nil {
+			if strings.Contains(err.Error(), "missing port in address") {
+				addressOnly = remoteAddr
+			} else {
+				server.Raise(err)
+			}
+		}
+	}
+
+	ipInfoRaw, err := controller.GetIPInfo(r.Context(), addressOnly)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	info := &ipinfo.Core{}
+	err = json.Unmarshal(ipInfoRaw, info)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// enrich the IP info with additional data
+	func(v *ipinfo.Core) {
+		if v.Country != "" {
+			v.CountryName = ipinfo.GetCountryName(v.Country)
+			v.IsEU = ipinfo.IsEU(v.Country)
+			v.CountryFlag.Emoji = ipinfo.GetCountryFlagEmoji(v.Country)
+			v.CountryFlag.Unicode = ipinfo.GetCountryFlagUnicode(v.Country)
+			v.CountryFlagURL = ipinfo.GetCountryFlagURL(v.Country)
+			v.CountryCurrency.Code = ipinfo.GetCountryCurrencyCode(v.Country)
+			v.CountryCurrency.Symbol = ipinfo.GetCountryCurrencySymbol(v.Country)
+			v.Continent.Code = ipinfo.GetContinentCode(v.Country)
+			v.Continent.Name = ipinfo.GetContinentName(v.Country)
+		}
+		if v.Abuse != nil && v.Abuse.Country != "" {
+			v.Abuse.CountryName = ipinfo.GetCountryName(v.Abuse.Country)
+		}
+	}(info)
+
+	if info.Bogon {
+		http.Error(w, "bogon IP", http.StatusForbidden)
+		return
+	}
+
+	myInfo, err := myinfo.FromIPInfo(info)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	isIPV6 := strings.Contains(addressOnly, ":")
+
+	var landmarksAndRTTs []landmarks.LandmarkAndRTT
+
+	if isIPV6 {
+		landmarksAndRTTs = landmarks.CurrentLandmarks.V6.ExpectedRTTSVerbose(*myInfo.Location.Coordinates, 5)
+	} else {
+		landmarksAndRTTs = landmarks.CurrentLandmarks.V4.ExpectedRTTSVerbose(*myInfo.Location.Coordinates, 5)
+	}
+
+	respStruct := response{
+		Info:               myInfo,
+		ExpectedRTTs:       landmarksAndRTTs,
+		ConnectedToNetwork: model.IsAddressConnectedToNetwork(r.Context(), addressOnly),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.Encode(respStruct)
+
+}
---
+++ v2025/model/gpt_model.go
@@ -0,0 +1,280 @@
+package model
+
+import (
+	"context"
+	"fmt"
+
+	// "strings"
+	"time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/session"
+)
+
+type CompletePrivacyPolicy struct {
+	PrivacyPolicyId   server.Id
+	ServiceName       string
+	ServiceUrls       []string
+	CreateTime        time.Time
+	Pending           bool
+	PrivacyPolicyText string
+	ExtractedUrls     []string
+}
+
+func NewCompletePrivacyPolicyPending(
+	serviceName string,
+	serviceUrls []string,
+) *CompletePrivacyPolicy {
+	return &CompletePrivacyPolicy{
+		ServiceName: serviceName,
+		ServiceUrls: serviceUrls,
+		CreateTime:  server.CodecTime(server.NowUtc()),
+		Pending:     true,
+	}
+}
+
+func NewCompletePrivacyPolicy(
+	serviceName string,
+	serviceUrls []string,
+	privacyPolicyText string,
+	extractedUrls []string,
+) *CompletePrivacyPolicy {
+	return &CompletePrivacyPolicy{
+		ServiceName:       serviceName,
+		ServiceUrls:       serviceUrls,
+		CreateTime:        server.CodecTime(server.NowUtc()),
+		Pending:           false,
+		PrivacyPolicyText: privacyPolicyText,
+		ExtractedUrls:     extractedUrls,
+	}
+}
+
+func GetCompletePrivacyPolicy(
+	ctx context.Context,
+	serviceName string,
+) (completePrivacyPolicy *CompletePrivacyPolicy, returnErr error) {
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+				SELECT privacy_policy_id
+				FROM latest_complete_privacy_policy
+				WHERE service_name = $1
+			`,
+			serviceName,
+		)
+
+		exists := false
+		var privacyPolicyId server.Id
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				exists = true
+				server.Raise(result.Scan(&privacyPolicyId))
+			}
+		})
+
+		if !exists {
+			returnErr = fmt.Errorf("Privacy policy does not exist for %s", serviceName)
+			return
+		}
+
+		completePrivacyPolicy = &CompletePrivacyPolicy{
+			PrivacyPolicyId: privacyPolicyId,
+			ServiceName:     serviceName,
+		}
+
+		result, err = conn.Query(
+			ctx,
+			`
+				SELECT
+					create_time,
+					privacy_policy_text,
+					pending
+				FROM complete_privacy_policy
+				WHERE privacy_policy_id = $1
+			`,
+			privacyPolicyId,
+		)
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				server.Raise(result.Scan(
+					&completePrivacyPolicy.CreateTime,
+					&completePrivacyPolicy.PrivacyPolicyText,
+					&completePrivacyPolicy.Pending,
+				))
+			}
+		})
+
+		result, err = conn.Query(
+			ctx,
+			`
+				SELECT service_url
+				FROM complete_privacy_policy_service_url
+				WHERE privacy_policy_id = $1
+			`,
+			privacyPolicyId,
+		)
+		server.WithPgResult(result, err, func() {
+			for result.Next() {
+				var serviceUrl string
+				server.Raise(result.Scan(&serviceUrl))
+				completePrivacyPolicy.ServiceUrls = append(
+					completePrivacyPolicy.ServiceUrls,
+					serviceUrl,
+				)
+			}
+		})
+
+		result, err = conn.Query(
+			ctx,
+			`
+				SELECT extracted_url
+				FROM complete_privacy_policy_extracted_url
+				WHERE privacy_policy_id = $1
+			`,
+			privacyPolicyId,
+		)
+		server.WithPgResult(result, err, func() {
+			for result.Next() {
+				var extractedUrl string
+				server.Raise(result.Scan(&extractedUrl))
+				completePrivacyPolicy.ExtractedUrls = append(
+					completePrivacyPolicy.ExtractedUrls,
+					extractedUrl,
+				)
+			}
+		})
+	})
+
+	return
+}
+
+func SetCompletePrivacyPolicy(ctx context.Context, completePrivacyPolicy *CompletePrivacyPolicy) {
+	server.Tx(ctx, func(tx server.PgTx) {
+		completePrivacyPolicy.PrivacyPolicyId = server.NewId()
+
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+				INSERT INTO complete_privacy_policy (
+					privacy_policy_id,
+					create_time,
+					service_name,
+					pending,
+					privacy_policy_text
+				) VALUES ($1, $2, $3, $4, $5)
+			`,
+			completePrivacyPolicy.PrivacyPolicyId,
+			completePrivacyPolicy.CreateTime,
+			completePrivacyPolicy.ServiceName,
+			completePrivacyPolicy.Pending,
+			completePrivacyPolicy.PrivacyPolicyText,
+		))
+
+		for _, serviceUrl := range completePrivacyPolicy.ServiceUrls {
+			server.RaisePgResult(tx.Exec(
+				ctx,
+				`
+					INSERT INTO complete_privacy_policy_service_url (
+						privacy_policy_id,
+						service_url
+					) VALUES ($1, $2)
+				`,
+				completePrivacyPolicy.PrivacyPolicyId,
+				serviceUrl,
+			))
+
+		}
+
+		for _, extractedUrl := range completePrivacyPolicy.ExtractedUrls {
+			server.RaisePgResult(tx.Exec(
+				ctx,
+				`
+					INSERT INTO complete_privacy_policy_extracted_url (
+						privacy_policy_id,
+						extracted_url
+					) VALUES ($1, $2)
+				`,
+				completePrivacyPolicy.PrivacyPolicyId,
+				extractedUrl,
+			))
+		}
+
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+				INSERT INTO latest_complete_privacy_policy (
+					service_name,
+					privacy_policy_id
+				) VALUES ($2, $1)
+				ON CONFLICT (service_name) DO UPDATE
+				SET privacy_policy_id = $1
+			`,
+			completePrivacyPolicy.PrivacyPolicyId,
+			completePrivacyPolicy.ServiceName,
+		))
+	})
+}
+
+type GptBeMyPrivacyAgentArgs struct {
+	CountryOfResidence  string                    `json:"country_of_residence"`
+	RegionOfResidence   string                    `json:"region_of_residence"`
+	CorrespondenceEmail string                    `json:"correspondence_email"`
+	Consent             bool                      `json:"consent"`
+	EmailText           *GptBeMyPrivacyAgentEmail `json:"email_text"`
+	ServiceName         string                    `json:"service_name"`
+	ServiceUser         string                    `json:"service_user"`
+}
+
+type GptBeMyPrivacyAgentEmail struct {
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Body    string `json:"body"`
+}
+
+type GptBeMyPrivacyAgentResult struct {
+	Accepted bool `json:"accepted"`
+}
+
+func SetGptBeMyPrivacyAgentPending(
+	beMyPrivacyAgent *GptBeMyPrivacyAgentArgs,
+	clientSession *session.ClientSession,
+) (*GptBeMyPrivacyAgentResult, error) {
+	accepted := true
+
+	server.Tx(clientSession.Ctx, func(tx server.PgTx) {
+		privacyAgentRequestId := server.NewId()
+
+		server.RaisePgResult(tx.Exec(
+			clientSession.Ctx,
+			`
+				INSERT INTO privacy_agent_request (
+					privacy_agent_request_id,
+		            country_of_residence,
+		            region_of_residence,
+		            correspondence_email,
+		            consent,
+		            email_text_to,
+		            email_text_subject,
+		            email_text_body,
+		            service_name,
+		            service_user
+				) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+			`,
+			privacyAgentRequestId,
+			beMyPrivacyAgent.CountryOfResidence,
+			beMyPrivacyAgent.RegionOfResidence,
+			beMyPrivacyAgent.CorrespondenceEmail,
+			beMyPrivacyAgent.Consent,
+			beMyPrivacyAgent.EmailText.To,
+			beMyPrivacyAgent.EmailText.Subject,
+			beMyPrivacyAgent.EmailText.Body,
+			beMyPrivacyAgent.ServiceName,
+			beMyPrivacyAgent.ServiceUser,
+		))
+	})
+
+	return &GptBeMyPrivacyAgentResult{
+		Accepted: accepted,
+	}, nil
+}
