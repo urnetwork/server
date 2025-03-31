d4b0e78d0715eb6452769edbc22d40285180aad9
Brien Colwell <xcolwell@gmail.com>
2025-03-31 16:11:59 -0700

    2025.3.31-58662681

---
+++ v2025/taskworker/work/subscription_work.go
@@ -0,0 +1,51 @@
+package work
+
+import (
+	"time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+	"github.com/urnetwork/server/v2025/task"
+)
+
+// Backfill initial transfer balance
+
+type BackfillInitialTransferBalanceArgs struct {
+}
+
+type BackfillInitialTransferBalanceResult struct {
+}
+
+func ScheduleBackfillInitialTransferBalance(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		BackfillInitialTransferBalance,
+		&BackfillInitialTransferBalanceArgs{},
+		clientSession,
+		task.RunOnce("backfill_initial_transfer_balance"),
+		task.RunAt(server.NowUtc().Add(15*time.Minute)),
+	)
+}
+
+func BackfillInitialTransferBalance(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	clientSession *session.ClientSession,
+) (*BackfillInitialTransferBalanceResult, error) {
+	networkIds := model.FindNetworksWithoutTransferBalance(clientSession.Ctx)
+	for _, networkId := range networkIds {
+		// add initial transfer balance
+		controller.AddRefreshTransferBalance(clientSession.Ctx, networkId)
+	}
+	return &BackfillInitialTransferBalanceResult{}, nil
+}
+
+func BackfillInitialTransferBalancePost(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	backfillInitialTransferBalanceResult *BackfillInitialTransferBalanceResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	return nil
+}
