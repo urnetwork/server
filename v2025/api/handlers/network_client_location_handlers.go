dce7f098950e512f9ace29fed004561e2accab46
Brien Colwell <xcolwell@gmail.com>
2025-03-31 16:25:33 -0700

    2025.3.31-58663496

---
+++ v2025/api/handlers/network_client_location_handlers.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/router"
+)
+
+func NetworkGetProviderLocations(w http.ResponseWriter, r *http.Request) {
+	router.WrapNoAuth(model.GetProviderLocations, w, r)
+}
+
+func NetworkFindProviderLocations(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(model.FindProviderLocations, w, r)
+}
+
+func NetworkFindLocations(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(model.FindLocations, w, r)
+}
+
+func NetworkFindProviders(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(model.FindProviders, w, r)
+}
+
+func NetworkFindProviders2(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(model.FindProviders2, w, r)
+}
+
+func NetworkCreateProviderSpec(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(model.CreateProviderSpec, w, r)
+}
---
+++ v2025/controller/network_user_controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+)
+
+type GetNetworkUserResult struct {
+	NetworkUser *model.NetworkUser         `json:"network_user,omitempty"`
+	Error       *GetNetworkUserResultError `json:"error,omitempty"`
+}
+
+type GetNetworkUserResultError struct {
+	Message string `json:"message"`
+}
+
+func GetNetworkUser(
+	clientSession *session.ClientSession,
+) (*GetNetworkUserResult, error) {
+
+	networkUser := model.GetNetworkUser(clientSession.Ctx, clientSession.ByJwt.UserId)
+	if networkUser == nil {
+		return &GetNetworkUserResult{
+			Error: &GetNetworkUserResultError{
+				Message: "No user found",
+			},
+		}, nil
+	}
+
+	return &GetNetworkUserResult{
+		NetworkUser: networkUser,
+	}, nil
+
+}
