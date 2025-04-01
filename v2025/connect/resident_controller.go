d97d9f4aede4241915ab0188e852f7b21b61d900
Brien Colwell <xcolwell@gmail.com>
2025-04-01 02:50:41 -0700

    2025.4.1-58701013

---
+++ v2025/connect/resident_controller.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	// "sync"
+	// "errors"
+	// "fmt"
+
+	// "crypto/hmac"
+	// "crypto/sha256"
+
+	// "google.golang.org/protobuf/proto"
+
+	"github.com/golang/glog"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+
+	// "github.com/urnetwork/connect/v2025"
+	"github.com/urnetwork/connect/protocol/v2025"
+)
+
+type residentController struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	clientId server.Id
+
+	residentContractManager *residentContractManager
+	settings                *ExchangeSettings
+}
+
+func newResidentController(
+	ctx context.Context,
+	cancel context.CancelFunc,
+	clientId server.Id,
+	residentContractManager *residentContractManager,
+	settings *ExchangeSettings,
+) *residentController {
+	return &residentController{
+		ctx:                     ctx,
+		cancel:                  cancel,
+		clientId:                clientId,
+		residentContractManager: residentContractManager,
+		settings:                settings,
+	}
+}
+
+// the frames are verified from source `clientId`
+// control messages are not allowed to have replies
+// messages with replies must use resident_oob_controller in the api
+func (self *residentController) HandleControlFrames(frames []*protocol.Frame) error {
+	outFrames, err := controller.ConnectControlFrames(
+		self.ctx,
+		self.clientId,
+		frames,
+	)
+	if err != nil {
+		return err
+	}
+
+	if 0 < len(outFrames) {
+		glog.Infof("[rr]dropped control reply frames: %d\n", len(outFrames))
+	}
+
+	return nil
+}
+
+// all controller activity moved to `controller.resident_oob_controller` via the api
