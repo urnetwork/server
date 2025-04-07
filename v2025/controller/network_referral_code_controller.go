0ff16f9f772ca97f346f5245b05d857e855fdc76
Brien Colwell <xcolwell@gmail.com>
2025-04-07 16:37:53 -0700

    2025.4.7-59269019

---
+++ v2025/controller/network_referral_code_controller.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+)
+
+type NetworkReferralResult struct {
+	ReferralCode   server.Id `json:"referral_code"`
+	TotalReferrals int       `json:"total_referrals"`
+}
+
+func GetNetworkReferralCode(
+	session *session.ClientSession,
+) (*NetworkReferralResult, error) {
+
+	res := model.GetNetworkReferralCode(session.Ctx, session.ByJwt.NetworkId)
+
+	networkReferralsResult := model.GetReferralsByReferralNetworkId(session.Ctx, session.ByJwt.NetworkId)
+
+	return &NetworkReferralResult{
+		ReferralCode:   res.ReferralCode,
+		TotalReferrals: len(networkReferralsResult),
+	}, nil
+
+}
+
+type ValidateNetworkReferralCodeResult struct {
+	IsValid bool `json:"is_valid"`
+}
+
+type ValidateReferralCodeArgs struct {
+	ReferralCode server.Id `json:"referral_code"`
+}
+
+/**
+ * When users manually enter a referral code, we want to show users whether it is valid or not.
+ */
+func ValidateReferralCode(
+	validateReferralCode *ValidateReferralCodeArgs,
+	session *session.ClientSession,
+) (*ValidateNetworkReferralCodeResult, error) {
+
+	isValid := model.ValidateReferralCode(session.Ctx, validateReferralCode.ReferralCode)
+
+	return &ValidateNetworkReferralCodeResult{
+		IsValid: isValid,
+	}, nil
+
+}
