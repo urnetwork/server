a158784416f77b4a9e22907d06f707a74f68ef05
Brien Colwell <xcolwell@gmail.com>
2025-04-09 13:39:39 -0700

    2025.4.9-59431135

---
+++ v2025/model/network_referral_code_model_test.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/urnetwork/server/v2025"
+)
+
+func TestNetworkReferralCode(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+
+		ctx := context.Background()
+		networkId := server.NewId()
+
+		// create a network referral code
+		createdReferralCode := CreateNetworkReferralCode(ctx, networkId)
+		assert.Equal(t, createdReferralCode.NetworkId, networkId)
+
+		// todo: add this back in when we switch to alphanumeric codes
+		// assert.Equal(t, len(createdReferralCode.ReferralCode), 6)
+
+		// get the network referral code
+		networkReferralCode := GetNetworkReferralCode(ctx, networkId)
+		assert.Equal(t, networkReferralCode.NetworkId, networkId)
+		assert.Equal(t, networkReferralCode.ReferralCode, createdReferralCode.ReferralCode)
+
+		// get the network id by referral code
+		referralNetworkId := GetNetworkIdByReferralCode(createdReferralCode.ReferralCode)
+		assert.Equal(t, referralNetworkId, networkId)
+
+		// validity checks
+		invalidReferralCode := "invalid_referral_code"
+		isValid := ValidateReferralCode(ctx, invalidReferralCode)
+		assert.Equal(t, isValid, false)
+
+		isValid = ValidateReferralCode(ctx, createdReferralCode.ReferralCode)
+		assert.Equal(t, isValid, true)
+
+	})
+}
