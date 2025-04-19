8e2f10f949f6c6a57a11d2655b0d915c672dd2bd
Product Builder <builder@bringyour.com>
2025-04-18 18:21:57 -0700

    2025.4.18-60225681

---
+++ v2025/model/auth_model_jwt.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/urnetwork/server/v2025/jwt"
+)
+
+type AuthJwt struct {
+	AuthType AuthType
+	UserAuth string
+	UserName string
+}
+
+func ParseAuthJwt(authJwt string, authJwtType AuthType) (*AuthJwt, error) {
+	switch authJwtType {
+	case AuthTypeApple:
+		appleJwt, err := jwt.ParseAppleJwt(authJwt)
+		if err != nil {
+			return nil, err
+		}
+		return &AuthJwt{
+			AuthType: AuthTypeApple,
+			UserAuth: appleJwt.UserAuth,
+			UserName: appleJwt.UserName,
+		}, nil
+	case AuthTypeGoogle:
+		googleJwt, err := jwt.ParseGoogleJwt(authJwt)
+		if err != nil {
+			return nil, err
+		}
+		return &AuthJwt{
+			AuthType: AuthTypeGoogle,
+			UserAuth: googleJwt.UserAuth,
+			UserName: googleJwt.UserName,
+		}, nil
+	}
+	return nil, fmt.Errorf("Unkown auth type: %s", authJwtType)
+}
