abad3d453af549cb352fceba9d51bfd03617d203
Product Builder <builder@bringyour.com>
2025-04-20 15:46:39 -0700

    2025.4.20-60389147

---
+++ v2025/api/handlers/subscription_handlers.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/router"
+)
+
+func SubscriptionBalance(w http.ResponseWriter, r *http.Request) {
+	router.WrapRequireAuth(controller.SubscriptionBalance, w, r)
+}
+
+func StripeWebhook(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputBodyFormatterNoAuth(
+		controller.VerifyStripeBody,
+		controller.StripeWebhook,
+		w,
+		r,
+	)
+}
+
+func CoinbaseWebhook(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputBodyFormatterNoAuth(
+		controller.VerifyCoinbaseBody,
+		controller.CoinbaseWebhook,
+		w,
+		r,
+	)
+}
+
+func PlayWebhook(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputBodyFormatterNoAuth(
+		controller.VerifyPlayBody,
+		controller.PlayWebhook,
+		w,
+		r,
+	)
+}
+
+func SubscriptionCheckBalanceCode(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputRequireAuth(model.CheckBalanceCode, w, r)
+}
+
+func SubscriptionRedeemBalanceCode(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputRequireAuth(model.RedeemBalanceCode, w, r)
+}
+
+func SubscriptionCreatePaymentId(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputRequireAuth(model.SubscriptionCreatePaymentId, w, r)
+}
