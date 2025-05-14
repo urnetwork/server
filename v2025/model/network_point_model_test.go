54003b7b9bed485b97031692ea8cc52785f09721
Brien Colwell <xcolwell@gmail.com>
2025-05-14 15:49:11 -0700

    2025.5.14-624628700

---
+++ v2025/model/network_point_model_test.go
@@ -0,0 +1,41 @@
+package model_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/model"
+)
+
+func TestNetworkPoints(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+
+		ctx := context.Background()
+		networkId := server.NewId()
+
+		err := model.ApplyNetworkPoints(ctx, networkId, model.NetworkPointEventReferral, 10)
+		assert.Equal(t, err, nil)
+
+		networkPoints := model.FetchNetworkPoints(ctx, networkId)
+		assert.NotEqual(t, networkPoints, nil)
+		assert.Equal(t, len(networkPoints), 1)
+		assert.Equal(t, networkPoints[0].NetworkId, networkId)
+		assert.Equal(t, networkPoints[0].Event, "referral")
+		assert.NotEqual(t, networkPoints[0].PointValue, 0)
+
+		err = model.ApplyNetworkPoints(ctx, networkId, model.NetworkPointEventReferral, 5)
+		assert.Equal(t, err, nil)
+
+		networkPoints = model.FetchNetworkPoints(ctx, networkId)
+		assert.Equal(t, len(networkPoints), 2)
+
+		totalPoints := 0
+		for _, point := range networkPoints {
+			totalPoints += point.PointValue
+		}
+		assert.Equal(t, totalPoints, 15)
+
+	})
+}
