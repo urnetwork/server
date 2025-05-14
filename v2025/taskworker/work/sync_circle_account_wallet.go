56a97cc6ade3913f09912e8e080caa62fc5ab1ea
Brien Colwell <xcolwell@gmail.com>
2025-05-14 16:01:50 -0700

    2025.5.14-624636600

---
+++ v2025/taskworker/work/sync_circle_account_wallet.go
@@ -0,0 +1,31 @@
+package work
+
+import (
+	"time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/session"
+	"github.com/urnetwork/server/v2025/task"
+)
+
+func SchedulePopulateAccountWallets(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		controller.PopulateAccountWallets,
+		&controller.PopulateAccountWalletsArgs{},
+		clientSession,
+		task.RunOnce("populate_circle_account_wallets"),
+		task.RunAt(time.Now().Add(24*time.Hour)),
+	)
+}
+
+func PopulateAccountWalletsPost(
+	populateAccountWallets *controller.PopulateAccountWalletsArgs,
+	populateAccountWalletsResult *controller.PopulateAccountWalletsResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	SchedulePopulateAccountWallets(clientSession, tx)
+	return nil
+}
