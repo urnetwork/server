7a96ef1c065705338bbc765374eb4d17c6d82a0b
Product Builder <builder@bringyour.com>
2025-06-03 01:18:31 -0700

    2025.6.3-641386110

---
+++ v2025/controller/account_point_controller_test.go
@@ -0,0 +1,60 @@
+package controller_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/jwt"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+)
+
+func TestAccountPoints(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+		ctx := context.Background()
+
+		networkId := server.NewId()
+		clientId := server.NewId()
+		userId := server.NewId()
+		networkName := "abcdef"
+
+		model.Testing_CreateNetwork(ctx, networkId, networkName, userId)
+
+		userSession := session.Testing_CreateClientSession(ctx, &jwt.ByJwt{
+			NetworkId: networkId,
+			ClientId:  &clientId,
+			UserId:    userId,
+		})
+
+		// assert no network points
+		result, err := controller.GetAccountPoints(userSession)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, len(result.AccountPoints), 0)
+
+		//  apply points
+		model.ApplyAccountPoints(ctx, networkId, "referral", 10)
+
+		// assert network points
+		result, err = controller.GetAccountPoints(userSession)
+		assert.Equal(t, err, nil)
+		assert.NotEqual(t, result.AccountPoints, nil)
+		assert.Equal(t, len(result.AccountPoints), 1)
+		assert.Equal(t, result.AccountPoints[0].NetworkId, networkId)
+		assert.Equal(t, result.AccountPoints[0].Event, "referral")
+		assert.NotEqual(t, result.AccountPoints[0].PointValue, 0)
+		assert.Equal(t, result.AccountPoints[0].CreateTime.IsZero(), false)
+		assert.Equal(t, result.AccountPoints[0].PointValue, 10)
+
+		// increment
+		model.ApplyAccountPoints(ctx, networkId, "referral", 5)
+
+		result, err = controller.GetAccountPoints(userSession)
+		assert.Equal(t, err, nil)
+		assert.NotEqual(t, result.AccountPoints, nil)
+		assert.Equal(t, len(result.AccountPoints), 2)
+
+	})
+}
