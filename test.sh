21db892f868eca07cbd4d46df3707b8c83382db0
Brien Colwell <xcolwell@gmail.com>
2025-06-18 20:27:55 -0700

    test: update

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ for d in `find . -iname '*_test.go' | xargs -n 1 dirname | sort | uniq | paste -
         export BRINGYOUR_POSTGRES_HOSTNAME="local-pg.bringyour.com"
         export BRINGYOUR_REDIS_HOSTNAME="local-redis.bringyour.com"
         # go test -v "$@" | grep --color=always -e "^" -e "$match"
-        GORACE="log_path=profile/race.out halt_on_error=1" go test -v -race -cpuprofile profile/cpu -memprofile profile/memory -timeout 180m "$@" | grep --color=always -e "^" -e "$match"
+        GORACE="log_path=profile/race.out halt_on_error=1" go test -timeout 0 -v -race -cpuprofile profile/cpu -memprofile profile/memory "$@" | grep --color=always -e "^" -e "$match"
         if [[ ${pipestatus[1]} != 0 ]]; then
             exit ${pipestatus[1]}
         fi
