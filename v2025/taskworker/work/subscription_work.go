fe867ea2e823c5b7e821a6e76f5a17cab9a01228
Product Builder <builder@bringyour.com>
2025-07-04 15:54:34 -0700

    2025.7.4-668695950

---
+++ v2025/taskworker/work/subscription_work.go
@@ -0,0 +1,130 @@
+package work
+
+import (
+	"time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+	"github.com/urnetwork/server/v2025/task"
+)
+
+type CloseExpiredContractsArgs struct {
+}
+
+type CloseExpiredContractsResult struct {
+}
+
+func ScheduleCloseExpiredContracts(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		CloseExpiredContracts,
+		&CloseExpiredContractsArgs{},
+		clientSession,
+		task.RunOnce("close_expired_contracts"),
+		task.RunAt(time.Now().Add(1*time.Hour)),
+	)
+}
+
+func CloseExpiredContracts(
+	closeExpiredContracts *CloseExpiredContractsArgs,
+	clientSession *session.ClientSession,
+) (*CloseExpiredContractsResult, error) {
+	err := model.ForceCloseOpenContractIds(clientSession.Ctx, 1*time.Hour)
+
+	return &CloseExpiredContractsResult{}, err
+}
+
+func CloseExpiredContractsPost(
+	closeExpiredContracts *CloseExpiredContractsArgs,
+	closeExpiredContractsResult *CloseExpiredContractsResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	ScheduleCloseExpiredContracts(clientSession, tx)
+	return nil
+}
+
+// Backfill initial transfer balance
+
+type BackfillInitialTransferBalanceArgs struct {
+}
+
+type BackfillInitialTransferBalanceResult struct {
+}
+
+func ScheduleBackfillInitialTransferBalance(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		BackfillInitialTransferBalance,
+		&BackfillInitialTransferBalanceArgs{},
+		clientSession,
+		task.RunOnce("backfill_initial_transfer_balance"),
+		task.RunAt(server.NowUtc().Add(15*time.Minute)),
+	)
+}
+
+func BackfillInitialTransferBalance(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	clientSession *session.ClientSession,
+) (*BackfillInitialTransferBalanceResult, error) {
+	networkIds := model.FindNetworksWithoutTransferBalance(clientSession.Ctx)
+	for _, networkId := range networkIds {
+		// add initial transfer balance
+		controller.AddRefreshTransferBalance(clientSession.Ctx, networkId)
+	}
+	return &BackfillInitialTransferBalanceResult{}, nil
+}
+
+func BackfillInitialTransferBalancePost(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	backfillInitialTransferBalanceResult *BackfillInitialTransferBalanceResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	return nil
+}
+
+type RemoveCompletedContractsArgs struct {
+}
+
+type RemoveCompletedContractsResult struct {
+}
+
+func ScheduleRemoveCompletedContracts(clientSession *session.ClientSession, tx server.PgTx) {
+	runAt := func() time.Time {
+		now := time.Now().UTC()
+		year, month, day := now.Date()
+		return time.Date(year, month, day+1, 0, 0, 0, 0, time.UTC)
+	}()
+
+	task.ScheduleTaskInTx(
+		tx,
+		RemoveCompletedContracts,
+		&RemoveCompletedContractsArgs{},
+		clientSession,
+		task.RunOnce("remove_completed_contracts"),
+		task.RunAt(runAt),
+		task.MaxTime(2*time.Hour),
+	)
+}
+
+func RemoveCompletedContracts(
+	removeCompletedContracts *RemoveCompletedContractsArgs,
+	clientSession *session.ClientSession,
+) (*RemoveCompletedContractsResult, error) {
+	minTime := time.Now().Add(-7 * 24 * time.Hour)
+	model.RemoveCompletedContracts(clientSession.Ctx, minTime)
+	return &RemoveCompletedContractsResult{}, nil
+}
+
+func RemoveCompletedContractsPost(
+	removeCompletedContracts *RemoveCompletedContractsArgs,
+	removeCompletedContractsResult *RemoveCompletedContractsResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	ScheduleRemoveCompletedContracts(clientSession, tx)
+	return nil
+}
