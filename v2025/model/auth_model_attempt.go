fb4e4cd098d80ec771d07427f46b6eca7c9c80d2
Product Builder <builder@bringyour.com>
2025-08-29 16:01:13 -0700

    2025.8.29-717084080

---
+++ v2025/model/auth_model_attempt.go
@@ -0,0 +1,170 @@
+package model
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	// "github.com/golang/glog"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/session"
+)
+
+func maxUserAuthAttemptsError() error {
+	return errors.New("User auth attempts exceeded limits.")
+}
+
+const AttemptLookback = 5 * time.Minute
+const AttemptFailedCountThreshold = 10
+
+func UserAuthAttempt(
+	userAuth *string,
+	session *session.ClientSession,
+) (userAuthAttemptId server.Id, allow bool) {
+	// insert attempt with success false
+	// select attempts by userAuth in past 5 minutes
+	// select attempts by clientIp in past 5 minutes
+	// if more than 10 failed in any, return false
+
+	clientAddressHash, clientPort, err := session.ClientAddressHashPort()
+	if err != nil {
+		return
+	}
+
+	server.Tx(session.Ctx, func(tx server.PgTx) {
+		userAuthAttemptId = server.NewId()
+
+		server.RaisePgResult(tx.Exec(
+			session.Ctx,
+			`
+				INSERT INTO user_auth_attempt
+				(user_auth_attempt_id, user_auth, client_address_hash, client_address_port, success)
+				VALUES ($1, $2, $3, $4, $5)
+			`,
+			userAuthAttemptId,
+			userAuth,
+			clientAddressHash[:],
+			clientPort,
+			false,
+		))
+
+		type UserAuthAttemptResult struct {
+			attemptTime time.Time
+			success     bool
+		}
+
+		parseAttempts := func(result server.PgResult) []UserAuthAttemptResult {
+			attempts := []UserAuthAttemptResult{}
+			for result.Next() {
+				var attempt UserAuthAttemptResult
+				server.Raise(result.Scan(
+					&attempt.attemptTime,
+					&attempt.success,
+				))
+				attempts = append(attempts, attempt)
+			}
+			return attempts
+		}
+
+		passesThreshold := func(attempts []UserAuthAttemptResult) bool {
+			failedCount := 0
+			for i := 0; i < len(attempts); i += 1 {
+				if !attempts[i].success {
+					failedCount += 1
+				}
+			}
+			return failedCount < AttemptFailedCountThreshold
+		}
+
+		if userAuth != nil {
+			// lookback by user auth
+			var attempts []UserAuthAttemptResult
+			result, err := tx.Query(
+				session.Ctx,
+				`
+					SELECT 
+						attempt_time,
+						success
+					FROM user_auth_attempt
+					WHERE 
+						user_auth = $1 AND
+						now() - INTERVAL '1 seconds' * $2 <= attempt_time AND
+						success = false
+					ORDER BY attempt_time DESC
+					LIMIT $3
+				`,
+				userAuth,
+				AttemptLookback/time.Second,
+				AttemptFailedCountThreshold,
+			)
+			server.WithPgResult(result, err, func() {
+				attempts = parseAttempts(result)
+			})
+			if !passesThreshold(attempts) {
+				return
+			}
+		}
+
+		var attempts []UserAuthAttemptResult
+		result, err := tx.Query(
+			session.Ctx,
+			`
+				SELECT 
+					attempt_time,
+					success
+				FROM user_auth_attempt
+				WHERE 
+					client_address_hash = $1 AND
+					now() - INTERVAL '1 seconds' * $2 <= attempt_time AND
+					success = false
+				ORDER BY attempt_time DESC
+				LIMIT $3
+			`,
+			clientAddressHash[:],
+			AttemptLookback/time.Second,
+			AttemptFailedCountThreshold,
+		)
+		server.WithPgResult(result, err, func() {
+			attempts = parseAttempts(result)
+		})
+		if !passesThreshold(attempts) {
+			return
+		}
+
+		allow = true
+	})
+	return
+}
+
+func SetUserAuthAttemptSuccess(
+	ctx context.Context,
+	userAuthAttemptId server.Id,
+	success bool,
+) {
+	server.Tx(ctx, func(tx server.PgTx) {
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+				UPDATE user_auth_attempt
+				SET success = $1
+				WHERE user_auth_attempt_id = $2
+			`,
+			success,
+			userAuthAttemptId,
+		))
+	})
+}
+
+func RemoveExpiredAuthAttempts(ctx context.Context, minTime time.Time) {
+	server.Tx(ctx, func(tx server.PgTx) {
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+				DELETE FROM user_auth_attempt
+				WHERE attempt_time < $1
+			`,
+			minTime.UTC(),
+		))
+	})
+}
---
+++ v2025/taskworker/work/subscription_work.go
@@ -0,0 +1,139 @@
+package work
+
+import (
+	"time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/controller"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+	"github.com/urnetwork/server/v2025/task"
+)
+
+type CloseExpiredContractsArgs struct {
+}
+
+type CloseExpiredContractsResult struct {
+}
+
+func ScheduleCloseExpiredContracts(clientSession *session.ClientSession, tx server.PgTx) {
+	runAt := func() time.Time {
+		now := server.NowUtc()
+		year, month, day := now.Date()
+		hour, minute, _ := now.Clock()
+		return time.Date(year, month, day, hour, 5*(minute/5)+5, 0, 0, time.UTC)
+	}()
+
+	task.ScheduleTaskInTx(
+		tx,
+		CloseExpiredContracts,
+		&CloseExpiredContractsArgs{},
+		clientSession,
+		// legacy key
+		task.RunOnce("close_expired_contracts"),
+		task.RunAt(runAt),
+		task.MaxTime(24*time.Hour),
+	)
+}
+
+func CloseExpiredContracts(
+	closeExpiredContracts *CloseExpiredContractsArgs,
+	clientSession *session.ClientSession,
+) (*CloseExpiredContractsResult, error) {
+	minTime := server.NowUtc().Add(-60 * time.Minute)
+	_, err := model.ForceCloseOpenContractIds(
+		clientSession.Ctx,
+		minTime,
+		1000000,
+		32,
+	)
+
+	return &CloseExpiredContractsResult{}, err
+}
+
+func CloseExpiredContractsPost(
+	closeExpiredContracts *CloseExpiredContractsArgs,
+	closeExpiredContractsResult *CloseExpiredContractsResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	ScheduleCloseExpiredContracts(clientSession, tx)
+	return nil
+}
+
+// Backfill initial transfer balance
+
+type BackfillInitialTransferBalanceArgs struct {
+}
+
+type BackfillInitialTransferBalanceResult struct {
+}
+
+func ScheduleBackfillInitialTransferBalance(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		BackfillInitialTransferBalance,
+		&BackfillInitialTransferBalanceArgs{},
+		clientSession,
+		task.RunOnce("backfill_initial_transfer_balance"),
+		task.RunAt(server.NowUtc().Add(15*time.Minute)),
+	)
+}
+
+func BackfillInitialTransferBalance(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	clientSession *session.ClientSession,
+) (*BackfillInitialTransferBalanceResult, error) {
+	networkIds := model.FindNetworksWithoutTransferBalance(clientSession.Ctx)
+	for _, networkId := range networkIds {
+		// add initial transfer balance
+		controller.AddRefreshTransferBalance(clientSession.Ctx, networkId)
+	}
+	return &BackfillInitialTransferBalanceResult{}, nil
+}
+
+func BackfillInitialTransferBalancePost(
+	backfillInitialTransferBalance *BackfillInitialTransferBalanceArgs,
+	backfillInitialTransferBalanceResult *BackfillInitialTransferBalanceResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	return nil
+}
+
+type RemoveCompletedContractsArgs struct {
+}
+
+type RemoveCompletedContractsResult struct {
+}
+
+func ScheduleRemoveCompletedContracts(clientSession *session.ClientSession, tx server.PgTx) {
+	task.ScheduleTaskInTx(
+		tx,
+		RemoveCompletedContracts,
+		&RemoveCompletedContractsArgs{},
+		clientSession,
+		task.RunOnce("remove_completed_contracts"),
+		task.RunAt(server.NowUtc().Add(1*time.Minute)),
+		task.MaxTime(30*time.Minute),
+	)
+}
+
+func RemoveCompletedContracts(
+	removeCompletedContracts *RemoveCompletedContractsArgs,
+	clientSession *session.ClientSession,
+) (*RemoveCompletedContractsResult, error) {
+	minTime := server.NowUtc().Add(-7 * 24 * time.Hour)
+	model.RemoveCompletedContracts(clientSession.Ctx, minTime)
+	return &RemoveCompletedContractsResult{}, nil
+}
+
+func RemoveCompletedContractsPost(
+	removeCompletedContracts *RemoveCompletedContractsArgs,
+	removeCompletedContractsResult *RemoveCompletedContractsResult,
+	clientSession *session.ClientSession,
+	tx server.PgTx,
+) error {
+	ScheduleRemoveCompletedContracts(clientSession, tx)
+	return nil
+}
