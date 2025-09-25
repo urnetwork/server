8ce451b6ba783d92a7659ebeb56cd48f731fe2a4
Product Builder <builder@bringyour.com>
2025-09-25 00:45:31 -0700

    2025.9.25-739862600

---
+++ v2025/controller/network_client_location_controller.go
@@ -0,0 +1,55 @@
+package controller
+
+import (
+	"context"
+	// "encoding/json"
+	// "net/http"
+	"net/netip"
+
+	// "encoding/base64"
+	// "fmt"
+	// "io"
+	// "sync"
+	// "time"
+
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/model"
+)
+
+func GetLocationForIp(ctx context.Context, clientIp string) (*model.Location, *model.ConnectionLocationScores, error) {
+	addr, err := netip.ParseAddr(clientIp)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ipInfo, err := server.GetIpInfo(addr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	location := &model.Location{
+		LocationType:  model.LocationTypeCity,
+		City:          ipInfo.City,
+		Region:        ipInfo.Region,
+		Country:       ipInfo.Country,
+		CountryCode:   ipInfo.CountryCode,
+		Continent:     ipInfo.Continent,
+		ContinentCode: ipInfo.ContinentCode,
+		Latitude:      ipInfo.Latitude,
+		Longitude:     ipInfo.Longitude,
+		Timezone:      ipInfo.Timezone,
+	}
+	location.LocationType, err = location.GuessLocationType()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	connectionLocationScores := &model.ConnectionLocationScores{}
+	switch ipInfo.UserType {
+	case server.UserTypeHosting:
+		connectionLocationScores.NetTypeHosting = 1
+		connectionLocationScores.NetTypeHosting2 = 1
+	}
+
+	return location, connectionLocationScores, nil
+}
