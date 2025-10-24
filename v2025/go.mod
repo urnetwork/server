698f9872da50a61fa4572a95dcc750f51b3fd177
Product Builder <builder@bringyour.com>
2025-10-24 15:17:07 -0700

    2025.10.24-765441650

---
+++ v2025/go.mod
@@ -0,0 +1,85 @@
+module github.com/urnetwork/server/v2025
+
+go 1.24.4
+
+toolchain go1.24.5
+
+require (
+	github.com/TwiN/go-away v1.8.0
+	github.com/aws/aws-sdk-go v1.55.8
+	github.com/coreos/go-semver v0.3.1
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/go-jose/go-jose/v3 v3.0.4
+	github.com/go-playground/assert/v2 v2.2.0
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/golang/glog v1.2.5
+	github.com/gorilla/websocket v1.5.3
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
+	github.com/jackc/pgx/v5 v5.7.6
+	github.com/mailgun/proxyproto v1.0.0
+	github.com/mozillazg/go-unidecode v0.2.0
+	github.com/nyaruka/phonenumbers v1.6.5
+	github.com/oklog/ulid/v2 v2.1.1
+	github.com/oschwald/maxminddb-golang/v2 v2.0.0-beta.10
+	github.com/pquerna/cachecontrol v0.2.0
+	github.com/prometheus/client_golang v1.22.0
+	github.com/quic-go/quic-go v0.55.0
+	github.com/redis/go-redis/v9 v9.14.0
+	github.com/samber/lo v1.51.0
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/stripe/stripe-go/v82 v82.5.1
+	github.com/tyler-smith/go-bip39 v1.1.0
+	golang.org/x/crypto v0.43.0
+	golang.org/x/exp v0.0.0-20251009144603-d2f985daa21b
+	golang.org/x/net v0.46.0
+	google.golang.org/protobuf v1.36.10
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/blendle/zapdriver v1.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/gagliardetto/binary v0.8.0 // indirect
+	github.com/gagliardetto/treeout v0.1.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mostynb/zstdpool-freelist v0.0.0-20201229113212-927304c0c3b1 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/streamingfast/logging v0.0.0-20250918142248-ac5a1e292845 // indirect
+	go.mongodb.org/mongo-driver v1.17.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/term v0.36.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gagliardetto/solana-go v1.13.0
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.63.0 // indirect
+	github.com/prometheus/procfs v0.16.0 // indirect
+	github.com/urnetwork/connect/v2025 v2025.10.24-765441650
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.37.0
+	golang.org/x/text v0.30.0 // indirect
+	src.agwa.name/tlshacks v0.0.0-20250628001001-c92050511ef4 // indirect
+)
---
+++ v2025/model/network_client_model_test.go
@@ -0,0 +1,198 @@
+package model
+
+import (
+	"context"
+	mathrand "math/rand"
+	"testing"
+	"time"
+
+	"github.com/go-playground/assert/v2"
+
+	"github.com/urnetwork/server/v2025"
+)
+
+func TestNetworkClientHandlerLifecycle(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+		ctx := context.Background()
+
+		clientId := server.NewId()
+
+		handlerId := CreateNetworkClientHandler(ctx)
+		connectionId, _, _, _, err := ConnectNetworkClient(
+			ctx,
+			clientId,
+			"0.0.0.0:0",
+			handlerId,
+		)
+		assert.Equal(t, err, nil)
+
+		err = HeartbeatNetworkClientHandler(ctx, handlerId)
+		assert.Equal(t, err, nil)
+
+		select {
+		case <-time.After(1 * time.Second):
+		}
+
+		connected := GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, true)
+
+		CloseExpiredNetworkClientHandlers(ctx, server.NowUtc())
+
+		connected = GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, false)
+
+		select {
+		case <-time.After(1 * time.Second):
+		}
+
+		RemoveDisconnectedNetworkClients(ctx, time.Now())
+
+		err = DisconnectNetworkClient(ctx, connectionId)
+		assert.NotEqual(t, err, nil)
+
+		err = HeartbeatNetworkClientHandler(ctx, handlerId)
+		assert.NotEqual(t, err, nil)
+	})
+}
+
+func TestNetworkClientHandlerLifecycleIPV6(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+		ctx := context.Background()
+
+		clientId := server.NewId()
+
+		handlerId := CreateNetworkClientHandler(ctx)
+		connectionId, _, _, _, err := ConnectNetworkClient(
+			ctx,
+			clientId,
+			"2001:5a8:4683:4e00:3a76:dcec:7cb:f180:40894",
+			handlerId,
+		)
+		assert.Equal(t, err, nil)
+
+		err = HeartbeatNetworkClientHandler(ctx, handlerId)
+		assert.Equal(t, err, nil)
+
+		time.Sleep(1 * time.Second)
+
+		connected := GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, true)
+
+		CloseExpiredNetworkClientHandlers(ctx, server.NowUtc())
+
+		connected = GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, false)
+
+		time.Sleep(1 * time.Second)
+
+		RemoveDisconnectedNetworkClients(ctx, time.Now())
+
+		err = DisconnectNetworkClient(ctx, connectionId)
+		assert.NotEqual(t, err, nil)
+
+		err = HeartbeatNetworkClientHandler(ctx, handlerId)
+		assert.NotEqual(t, err, nil)
+	})
+}
+
+func TestNetworkClientLifecycle(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+		ctx := context.Background()
+
+		clientId := server.NewId()
+
+		handlerId := CreateNetworkClientHandler(ctx)
+		connectionId, _, _, _, err := ConnectNetworkClient(
+			ctx,
+			clientId,
+			"0.0.0.0:0",
+			handlerId,
+		)
+		assert.Equal(t, err, nil)
+
+		select {
+		case <-time.After(1 * time.Second):
+		}
+
+		connected := GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, true)
+
+		err = DisconnectNetworkClient(ctx, connectionId)
+		assert.Equal(t, err, nil)
+
+		connected = GetNetworkClientConnectionStatus(ctx, connectionId).Connected
+		assert.Equal(t, connected, false)
+
+		RemoveDisconnectedNetworkClients(ctx, time.Now())
+
+		err = DisconnectNetworkClient(ctx, connectionId)
+		assert.NotEqual(t, err, nil)
+	})
+}
+
+// FIXME test GetNetworkClients, SetPendingNetworkClientConnection
+
+func TestSetProvide(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		newSecretKeys := func() map[ProvideMode][]byte {
+			k := make([]byte, 32)
+			mathrand.Read(k)
+			return map[ProvideMode][]byte{
+				ProvideModePublic: k,
+			}
+		}
+
+		clientId := server.NewId()
+		secretKeys := newSecretKeys()
+
+		startTime := server.NowUtc()
+
+		changeCount, provideModes := GetProvideKeyChanges(ctx, clientId, startTime)
+		assert.Equal(t, changeCount, 0)
+		assert.Equal(t, provideModes, map[ProvideMode]bool{})
+
+		for provideMode, _ := range secretKeys {
+			_, err := GetProvideSecretKey(ctx, clientId, provideMode)
+			assert.NotEqual(t, err, nil)
+		}
+
+		SetProvide(ctx, clientId, secretKeys)
+
+		for provideMode, secretKey := range secretKeys {
+			k, err := GetProvideSecretKey(ctx, clientId, provideMode)
+			assert.Equal(t, err, nil)
+			assert.Equal(t, k, secretKey)
+		}
+
+		changeCount, provideModes = GetProvideKeyChanges(ctx, clientId, startTime)
+		assert.Equal(t, changeCount, 1)
+		assert.Equal(t, provideModes, map[ProvideMode]bool{
+			ProvideModePublic: true,
+		})
+
+		n := 32
+		for range n {
+			secretKeys = newSecretKeys()
+			SetProvide(ctx, clientId, secretKeys)
+		}
+
+		changeCount, provideModes = GetProvideKeyChanges(ctx, clientId, startTime)
+		assert.Equal(t, changeCount, n+1)
+		assert.Equal(t, provideModes, map[ProvideMode]bool{
+			ProvideModePublic: true,
+		})
+
+		RemoveOldProvideKeyChanges(ctx, server.NowUtc())
+
+		changeCount, provideModes = GetProvideKeyChanges(ctx, clientId, startTime)
+		assert.Equal(t, changeCount, 0)
+		assert.Equal(t, provideModes, map[ProvideMode]bool{
+			ProvideModePublic: true,
+		})
+
+	})
+}
