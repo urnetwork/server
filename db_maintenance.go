aeed4c8d188938bc988638c1afc5ce90cc73c177
Brien Colwell <xcolwell@gmail.com>
2025-11-08 11:27:55 -0600

    db: maintenance add index cleanup

--- db_maintenance.go
+++ db_maintenance.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"hash/maphash"
 	mathrand "math/rand"
+	"regexp"
 	"slices"
 	"strings"
 
@@ -18,6 +19,56 @@ func DbMaintenance(ctx context.Context, epoch uint64) {
 	// 1. tables are reindexed over `DbReindexEpochs` epochs
 	//    e.g. `DbReindexEpochs=4` means all tables will be reindexed over 4 maintenance epochs
 	// 2. ANALYZE is called after each maintenance to update the planner stats
+
+	// note `REINDEX CONCURRENTLY` can be safely run in the background
+	// see https://www.postgresql.org/docs/current/sql-reindex.html
+
+	cleanUpIncompleteIdexes := func(conn PgConn, tableName string) {
+		// per the posgres docs, remove indexes that end in _ccnew\d* or _ccold\d*
+		incompleteIndexNamePattern := regexp.MustCompile("^(?:.*_ccnew\\d*|.*_ccold\\d*)$")
+
+		incompleteIndexNames := []string{}
+
+		result, err := conn.Query(
+			ctx,
+			`
+				SELECT
+				    pg_class.relname AS index_name
+				FROM
+				    pg_class
+				INNER JOIN
+				    pg_index ON pg_index.indexrelid = pg_class.oid
+				INNER JOIN
+				    pg_class t ON t.oid = pg_index.indrelid
+				WHERE
+				    pg_index.indisvalid = false AND
+				    t.relname = $1
+			`,
+			tableName,
+		)
+		WithPgResult(result, err, func() {
+			var indexName string
+			Raise(result.Scan(&indexName))
+			if incompleteIndexNamePattern.MatchString(indexName) {
+				incompleteIndexNames = append(incompleteIndexNames, indexName)
+			}
+		})
+
+		if 0 < len(incompleteIndexNames) {
+
+			for _, incompleteIndexName := range incompleteIndexNames {
+				glog.Infof("[db]maintenance found incomplete index %s on table %s\n", incompleteIndexName, tableName)
+				// FIXME for now just report the indexex, do not drop
+				// RaisePgResult(conn.Exec(
+				// 	ctx,
+				// 	`
+				// 	DROP INDEX $1
+				// 	`,
+				// 	incompleteIndexName,
+				// ))
+			}
+		}
+	}
 
 	Db(ctx, func(conn PgConn) {
 		tableNames := []string{}
@@ -61,6 +112,8 @@ func DbMaintenance(ctx context.Context, epoch uint64) {
 		// reindex concurrently
 		for i, reindexTableName := range reindexTableNames {
 			glog.Infof("[db]maintenance reindex[%d/%d] %s\n", i+1, len(reindexTableNames), reindexTableName)
+
+			// reindex
 			RaisePgResult(conn.Exec(
 				ctx,
 				`
@@ -68,6 +121,8 @@ func DbMaintenance(ctx context.Context, epoch uint64) {
 				`,
 				reindexTableName,
 			))
+
+			cleanUpIncompleteIdexes(conn, reindexTableName)
 		}
 
 		// final analyze
