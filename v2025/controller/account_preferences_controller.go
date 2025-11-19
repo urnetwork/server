ddfe690f6844833b26131b0d0eacbb33582ed396
Product Builder <builder@bringyour.com>
2025-11-19 14:36:14 -0800

    2025.11.19-787917120

---
+++ v2025/controller/account_preferences_controller.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"github.com/urnetwork/server/v2025"
+	"github.com/urnetwork/server/v2025/model"
+	"github.com/urnetwork/server/v2025/session"
+)
+
+func AccountPreferencesGet(session *session.ClientSession) (*model.AccountPreferencesGetResult, error) {
+	return model.AccountPreferencesGet(session), nil
+}
+
+func AccountPreferencesSet(
+	preferencesSet *model.AccountPreferencesSetArgs,
+	session *session.ClientSession,
+) (*model.AccountPreferencesSetResult, error) {
+	r, err := model.AccountPreferencesSet(preferencesSet, session)
+	if err == nil {
+		server.Tx(session.Ctx, func(tx server.PgTx) {
+			ScheduleSyncProductUpdatesForUser(
+				session,
+				session.ByJwt.UserId,
+				tx,
+			)
+		})
+	}
+	return r, err
+}
