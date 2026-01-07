3ad04300b5f64ffd295e8a02d765967cbccacd9f
Product Builder <builder@bringyour.com>
2026-01-07 00:11:16 -0800

    2026.1.7-829733830

---
+++ v2026/connect/resident_contract_manager.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	// "fmt"
+
+	// "golang.org/x/exp/maps"
+
+	"github.com/urnetwork/server/v2026"
+	"github.com/urnetwork/server/v2026/model"
+)
+
+type residentContractManager struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	clientId server.Id
+
+	settings *ExchangeSettings
+
+	stateLock       sync.Mutex
+	activeContracts map[model.TransferPair]*activeContractEntry
+}
+
+func newResidentContractManager(
+	ctx context.Context,
+	cancel context.CancelFunc,
+	clientId server.Id,
+	settings *ExchangeSettings,
+) *residentContractManager {
+	residentContractManager := &residentContractManager{
+		ctx:             ctx,
+		cancel:          cancel,
+		clientId:        clientId,
+		settings:        settings,
+		activeContracts: map[model.TransferPair]*activeContractEntry{},
+	}
+
+	return residentContractManager
+}
+
+// all other controller activity moved to `controller.resident_oob_controller` via the api
+
+func (self *residentContractManager) HasActiveContract(sourceId server.Id, destinationId server.Id) bool {
+	transferPair := model.NewTransferPair(sourceId, destinationId)
+
+	// entry is either not expired or nil
+	var entry *activeContractEntry
+	refresh := false
+
+	if 0 < self.settings.ContractManagerCheckTimeout {
+		func() {
+			self.stateLock.Lock()
+			defer self.stateLock.Unlock()
+
+			var ok bool
+			entry, ok = self.activeContracts[transferPair]
+			if ok {
+				if entry.checkTime.Add(self.settings.ContractManagerCheckTimeout).Before(time.Now()) {
+					entry = nil
+				} else if !entry.refresh && entry.checkTime.Add(self.settings.ContractManagerCheckTimeout/2).Before(time.Now()) {
+					entry.refresh = true
+					refresh = true
+				}
+			}
+		}()
+	}
+
+	next := func() (nextEntry *activeContractEntry) {
+		c := func() bool {
+			contractIds1 := model.GetOpenContractIdsWithNoPartialClose(self.ctx, sourceId, destinationId)
+			if 0 < len(contractIds1) {
+				return true
+			}
+
+			contractIds2 := model.GetOpenContractIdsWithNoPartialClose(self.ctx, destinationId, sourceId)
+			if 0 < len(contractIds2) {
+				return true
+			}
+
+			return false
+		}
+		hasActiveContract := c()
+
+		func() {
+			self.stateLock.Lock()
+			defer self.stateLock.Unlock()
+			if hasActiveContract {
+				nextEntry = &activeContractEntry{
+					checkTime: time.Now(),
+					refresh:   false,
+				}
+				self.activeContracts[transferPair] = nextEntry
+			} else {
+				delete(self.activeContracts, transferPair)
+			}
+		}()
+		return
+	}
+
+	if entry == nil {
+		entry = next()
+	} else if refresh {
+		go next()
+	}
+
+	return entry != nil
+}
+
+type activeContractEntry struct {
+	checkTime time.Time
+	refresh   bool
+}
---
+++ v2026/router/router.go
@@ -0,0 +1,101 @@
+package router
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"regexp"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/urnetwork/glog/v2026"
+
+	"github.com/urnetwork/server/v2026"
+)
+
+// regex table approach inspired by https://benhoyt.com/writings/go-routing/
+
+type Route struct {
+	method  string
+	regex   *regexp.Regexp
+	handler http.HandlerFunc
+}
+
+func NewRoute(method string, pattern string, handler http.HandlerFunc) *Route {
+	return &Route{
+		method:  method,
+		regex:   regexp.MustCompile("^" + pattern + "$"),
+		handler: handler,
+	}
+}
+
+func (self *Route) String() string {
+	return fmt.Sprintf("%s %s", self.method, self.regex)
+}
+
+type pathValuesKey struct{}
+
+type Router struct {
+	ctx    context.Context
+	routes []*Route
+	stats  *RouterStats
+}
+
+func NewRouter(ctx context.Context, routes []*Route) *Router {
+	return &Router{
+		ctx:    ctx,
+		routes: routes,
+		stats:  NewRouterStats(ctx, 60*time.Second),
+	}
+}
+
+// `http.Handler`
+func (self *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var allow []string
+	for _, route := range self.routes {
+		matches := route.regex.FindStringSubmatch(r.URL.Path)
+		if 0 < len(matches) {
+			if r.Method != route.method {
+				allow = append(allow, route.method)
+				continue
+			}
+			ctx := context.WithValue(r.Context(), pathValuesKey{}, matches[1:])
+			defer func() {
+				if r := recover(); r != nil {
+					// suppress the error
+					if server.IsDoneError(r) {
+						// standard pattern to raise on context done. ignore
+					} else {
+						self.stats.Error(route.String())
+						glog.Infof(
+							"[h]unhandled error from route %s: %s\n",
+							route.String(),
+							server.ErrorJson(r, debug.Stack()),
+						)
+					}
+					func() {
+						// note the connection might be hijacked in this case
+						defer recover()
+						http.Error(w, "Error. Please email support@ur.io for help.", http.StatusInternalServerError)
+					}()
+				}
+			}()
+			startTime := time.Now()
+			route.handler(w, r.WithContext(ctx))
+			endTime := time.Now()
+			self.stats.Success(route.String(), endTime.Sub(startTime))
+			return
+		}
+	}
+	if 0 < len(allow) {
+		w.Header().Set("Allow", strings.Join(allow, ", "))
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+	http.NotFound(w, r)
+}
+
+func GetPathValues(r *http.Request) []string {
+	return r.Context().Value(pathValuesKey{}).([]string)
+}
