271eb14dd8bd4bbe8d3aaa4728459327ff2bc740
Product Builder <builder@bringyour.com>
2026-01-14 11:39:36 -0800

    2026.1.14-836194520

---
+++ v2026/connect/resident_proxy.go
@@ -0,0 +1,188 @@
+package main
+
+// FIXME have a multi client in the proxy, set proxy as the generator
+
+import (
+	"context"
+	// "fmt"
+	"time"
+
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/connect/v2026/protocol"
+	"github.com/urnetwork/sdk/v2026"
+	"github.com/urnetwork/server/v2026"
+	"github.com/urnetwork/server/v2026/jwt"
+	"github.com/urnetwork/server/v2026/model"
+)
+
+func DefaultResidentProxyDeviceSettings() *ResidentProxyDeviceSettings {
+	return &ResidentProxyDeviceSettings{
+		ProxyDeviceDescription: "resident proxy",
+		ProxyDeviceSpec:        "resident proxy",
+	}
+}
+
+type ResidentProxyDeviceSettings struct {
+	ProxyDeviceDescription         string
+	ProxyDeviceSpec                string
+	IngressSecurityPolicyGenerator func(*connect.SecurityPolicyStatsCollector) connect.SecurityPolicy
+	EgressSecurityPolicyGenerator  func(*connect.SecurityPolicyStatsCollector) connect.SecurityPolicy
+}
+
+type ResidentProxyDevice struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	exchange          *Exchange
+	clientId          server.Id
+	instanceId        server.Id
+	proxyDeviceConfig *model.ProxyDeviceConfig
+
+	deviceLocal *sdk.DeviceLocal
+	settings    *ResidentProxyDeviceSettings
+}
+
+func NewResidentProxyDeviceWithDefaults(
+	ctx context.Context,
+	exchange *Exchange,
+	clientId server.Id,
+	instanceId server.Id,
+	proxyDeviceConfig *model.ProxyDeviceConfig,
+) (*ResidentProxyDevice, error) {
+	return NewResidentProxyDevice(
+		ctx,
+		exchange,
+		clientId,
+		instanceId,
+		proxyDeviceConfig,
+		DefaultResidentProxyDeviceSettings(),
+	)
+}
+
+func NewResidentProxyDevice(
+	ctx context.Context,
+	exchange *Exchange,
+	clientId server.Id,
+	instanceId server.Id,
+	proxyDeviceConfig *model.ProxyDeviceConfig,
+	settings *ResidentProxyDeviceSettings,
+) (*ResidentProxyDevice, error) {
+
+	// this jwt is used to access the services in the network space
+	byJwt, err := jwt.LoadByJwtFromClientId(ctx, clientId)
+	if err != nil {
+		return nil, err
+	}
+
+	cancelCtx, cancel := context.WithCancel(ctx)
+
+	networkSpace := sdk.NewPlatformNetworkSpace(ctx, server.RequireEnv(), server.RequireHost())
+
+	generatorFunc := func(specs []*connect.ProviderSpec) connect.MultiClientGenerator {
+		return newExchangeGenerator(
+			cancelCtx,
+			exchange,
+			byJwt,
+			specs,
+			[]server.Id{clientId},
+			clientId,
+			connect.DefaultClientSettings,
+			settings,
+		)
+	}
+
+	deviceLocal, err := sdk.NewPlatformDeviceLocalWithDefaults(
+		generatorFunc,
+		networkSpace,
+		byJwt.Sign(),
+		settings.ProxyDeviceDescription,
+		settings.ProxyDeviceSpec,
+		server.RequireVersion(),
+		sdk.RequireIdFromBytes(instanceId.Bytes()),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if settings.IngressSecurityPolicyGenerator != nil {
+		deviceLocal.SetIngressSecurityPolicyGenerator(settings.IngressSecurityPolicyGenerator)
+	}
+	if settings.EgressSecurityPolicyGenerator != nil {
+		deviceLocal.SetEgressSecurityPolicyGenerator(settings.EgressSecurityPolicyGenerator)
+	}
+
+	if initialDeviceState := proxyDeviceConfig.InitialDeviceState; initialDeviceState != nil {
+		deviceLocal.SetPerformanceProfile(initialDeviceState.PerformanceProfile)
+		deviceLocal.SetConnectLocation(initialDeviceState.Location)
+	}
+
+	proxyDevice := &ResidentProxyDevice{
+		ctx:               cancelCtx,
+		cancel:            cancel,
+		exchange:          exchange,
+		clientId:          clientId,
+		proxyDeviceConfig: proxyDeviceConfig,
+		deviceLocal:       deviceLocal,
+		settings:          settings,
+	}
+
+	return proxyDevice, nil
+}
+
+func (self *ResidentProxyDevice) AddTun() (
+	send chan []byte,
+	receive chan []byte,
+	closeTun func(),
+) {
+	send = make(chan []byte, self.exchange.settings.ExchangeBufferSize)
+	receive = make(chan []byte, self.exchange.settings.ExchangeBufferSize)
+
+	tunCtx, tunCancel := context.WithCancel(self.ctx)
+
+	go server.HandleError(func() {
+		defer tunCancel()
+		for {
+			select {
+			case <-tunCtx.Done():
+				return
+			case packet, ok := <-receive:
+				if !ok {
+					return
+				}
+				self.deviceLocal.SendPacketNoCopy(packet, int32(len(packet)))
+			case <-time.After(self.exchange.settings.WriteTimeout):
+				// drop
+			}
+		}
+	})
+
+	receiveCallback := func(source connect.TransferPath, provideMode protocol.ProvideMode, ipPath *connect.IpPath, packet []byte) {
+		select {
+		case <-tunCtx.Done():
+			return
+		case send <- packet:
+		case <-time.After(self.exchange.settings.WriteTimeout):
+		}
+	}
+	unsub := self.deviceLocal.AddReceivePacketCallback(receiveCallback)
+
+	closeTun = func() {
+		tunCancel()
+		unsub()
+
+		// note `send` is not closed. This channel is left open.
+	}
+
+	return
+}
+
+func (self *ResidentProxyDevice) Close() {
+	self.cancel()
+
+	self.deviceLocal.Close()
+}
+
+// func newExchangeNetworkSpace(exchange *Exchange) *sdk.NetworkSpace {
+// 	// FIXME
+// 	return nil
+
+// }
---
+++ v2026/model/network_client_proxy_model.go
@@ -0,0 +1,402 @@
+package model
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base32"
+	"encoding/json"
+	"fmt"
+	"net/netip"
+	"slices"
+	"strings"
+	"sync"
+
+	// "github.com/urnetwork/glog/v2026"
+
+	"github.com/urnetwork/sdk/v2026"
+	"github.com/urnetwork/server/v2026"
+)
+
+func base32Encoder() *base32.Encoding {
+	return base32.HexEncoding.WithPadding(base32.NoPadding)
+}
+
+// ordered in precedence
+var proxySigningSecrets = sync.OnceValue(func() [][]byte {
+	proxy := server.Vault.RequireSimpleResource("proxy.yml")
+	secretStrs := proxy.RequireStringList("secrets")
+	var secrets [][]byte
+	for _, secretStr := range secretStrs {
+		secrets = append(secrets, []byte(secretStr))
+	}
+	return secrets
+})
+
+var proxySigningSecret = sync.OnceValue(func() []byte {
+	return proxySigningSecrets()[0]
+})
+
+// the signed proxy id is intended to use in the proxy hostname,
+// to make it hard to guess a proxy id (160 bits of entropy),
+// and to allow the server to fast reject a request
+// the returned length is <63 alphanumeric characters
+func SignProxyId(proxyId server.Id) string {
+	proxyIdBytes := proxyId.Bytes()
+
+	secret := proxySigningSecret()
+
+	h := hmac.New(sha1.New, secret)
+	h.Write(proxyIdBytes)
+	signature := h.Sum(nil)
+
+	e := base32Encoder()
+	var b []byte
+	b = append(b, proxyIdBytes...)
+	b = append(b, signature...)
+	return e.EncodeToString(b)
+}
+
+func ParseSignedProxyId(signedProxyId string) (proxyId server.Id, returnErr error) {
+	e := base32Encoder()
+	b, err := e.DecodeString(strings.ToUpper(signedProxyId))
+
+	if err != nil {
+		returnErr = err
+		return
+	}
+
+	if len(b) < 16 {
+		returnErr = fmt.Errorf("Invalid input length")
+		return
+	}
+
+	proxyId, returnErr = server.IdFromBytes(b[0:16])
+	if returnErr != nil {
+		return
+	}
+	signature := b[16:]
+
+	// validate the signature with all known secrets
+	ok := func() bool {
+		proxyIdBytes := proxyId.Bytes()
+		for _, secret := range proxySigningSecrets() {
+			h := hmac.New(sha1.New, secret)
+			h.Write(proxyIdBytes)
+			checkSignature := h.Sum(nil)
+			if slices.Equal(signature, checkSignature) {
+				return true
+			}
+		}
+		return false
+	}()
+	if !ok {
+		returnErr = fmt.Errorf("Invalid signature")
+		return
+	}
+
+	return
+}
+
+func EncodeProxyId(proxyId server.Id) string {
+	proxyIdBytes := proxyId.Bytes()
+
+	e := base32Encoder()
+	var b []byte
+	b = append(b, proxyIdBytes...)
+	return e.EncodeToString(b)
+}
+
+func ParseEncodedProxyId(encodedProxyId string) (proxyId server.Id, returnErr error) {
+	e := base32Encoder()
+	b, err := e.DecodeString(strings.ToUpper(encodedProxyId))
+
+	if err != nil {
+		returnErr = err
+		return
+	}
+
+	if len(b) < 16 {
+		returnErr = fmt.Errorf("Invalid input length")
+		return
+	}
+
+	proxyId, returnErr = server.IdFromBytes(b[0:16])
+	return
+}
+
+func RequireEncodedProxyId(encodedProxyId string) server.Id {
+	proxyId, err := ParseEncodedProxyId(encodedProxyId)
+	if err != nil {
+		panic(err)
+	}
+	return proxyId
+}
+
+type ProxyDeviceMode int
+
+const (
+	ProxyDeviceModeDevice ProxyDeviceMode = 1
+)
+
+type ProxyDeviceConnection struct {
+	ProxyId    server.Id `json:"proxy_id"`
+	ClientId   server.Id `json:"client_id"`
+	InstanceId server.Id `json:"instance_id"`
+}
+
+type ProxyDeviceConfig struct {
+	ProxyDeviceConnection
+	ProxyDeviceMode ProxyDeviceMode `json:"proxy_device_mode"`
+	LockSubnets     []netip.Prefix  `json:"lock_subnets"`
+	HttpRequireAuth bool            `json:"http_require_auth"`
+
+	InitialDeviceState *ProxyDeviceState `json:"initial_device_state"`
+}
+
+type ProxyDeviceState struct {
+	Location *sdk.ConnectLocation `json:"location"`
+
+	PerformanceProfile *sdk.PerformanceProfile `json:"performance_profile"`
+}
+
+func GetProxyDeviceConnection(ctx context.Context, proxyId server.Id) (proxyDeviceConnection *ProxyDeviceConnection) {
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+			SELECT
+				client_id,
+				instance_id
+			FROM proxy_device_config
+			WHERE proxy_id = $1
+			`,
+			proxyId,
+		)
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				var clientId server.Id
+				var instanceId server.Id
+				server.Raise(result.Scan(&clientId, &instanceId))
+				proxyDeviceConnection = &ProxyDeviceConnection{
+					ProxyId:    proxyId,
+					ClientId:   clientId,
+					InstanceId: instanceId,
+				}
+			}
+		})
+	})
+	return
+}
+
+func GetProxyDeviceConnectionForClient(
+	ctx context.Context,
+	clientId server.Id,
+	instanceId server.Id,
+) (proxyDeviceConnection *ProxyDeviceConnection) {
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+			SELECT
+				proxy_id
+			FROM proxy_device_config
+			WHERE
+				client_id = $1 AND
+				instance_id = $2
+			`,
+			clientId,
+			instanceId,
+		)
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				var proxyId server.Id
+				server.Raise(result.Scan(&clientId, &instanceId))
+				proxyDeviceConnection = &ProxyDeviceConnection{
+					ProxyId:    proxyId,
+					ClientId:   clientId,
+					InstanceId: instanceId,
+				}
+			}
+		})
+	})
+	return
+}
+
+func CreateProxyDeviceConfig(ctx context.Context, proxyDeviceConfig *ProxyDeviceConfig) (returnErr error) {
+
+	server.Tx(ctx, func(tx server.PgTx) {
+		proxyDeviceConfig.ProxyId = server.NewId()
+		proxyDeviceConfig.InstanceId = server.NewId()
+
+		proxyDeviceConfigJson, err := json.Marshal(proxyDeviceConfig)
+		if err != nil {
+			returnErr = err
+			return
+		}
+
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+			INSERT INTO proxy_device_config (
+				proxy_id,
+				client_id,
+				instance_id,
+				config_json
+			)
+			VALUES ($1, $2, $3, $4)
+			`,
+			proxyDeviceConfig.ProxyId,
+			proxyDeviceConfig.ClientId,
+			proxyDeviceConfig.InstanceId,
+			proxyDeviceConfigJson,
+		))
+	})
+	return nil
+}
+
+func RemoveProxyDeviceConfig(ctx context.Context, proxyId server.Id) {
+
+	server.Tx(ctx, func(tx server.PgTx) {
+		server.RaisePgResult(tx.Exec(
+			ctx,
+			`
+			DELETE FROM proxy_device_config
+			WHERE proxy_id = $1
+			`,
+			proxyId,
+		))
+	})
+
+}
+
+func GetProxyDeviceConfig(ctx context.Context, proxyId server.Id) *ProxyDeviceConfig {
+	var proxyDeviceConfigJson string
+
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+			SELECT
+				config_json
+			FROM proxy_device_config
+			WHERE
+				proxy_id = $1
+			`,
+			proxyId,
+		)
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				server.Raise(result.Scan(&proxyDeviceConfigJson))
+			}
+		})
+	})
+
+	if proxyDeviceConfigJson == "" {
+		return nil
+	}
+
+	var proxyDeviceConfig ProxyDeviceConfig
+	err := json.Unmarshal([]byte(proxyDeviceConfigJson), &proxyDeviceConfig)
+	if err != nil {
+		return nil
+	}
+	return &proxyDeviceConfig
+}
+
+func GetProxyDeviceConfigForClient(ctx context.Context, clientId server.Id, instanceId server.Id) *ProxyDeviceConfig {
+	var proxyDeviceConfigJson string
+
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+			SELECT
+				config_json
+			FROM proxy_device_config
+			WHERE
+				client_id = $1 AND
+				instance_id = $2
+			`,
+			clientId,
+			instanceId,
+		)
+		server.WithPgResult(result, err, func() {
+			if result.Next() {
+				server.Raise(result.Scan(&proxyDeviceConfigJson))
+			}
+		})
+	})
+
+	if proxyDeviceConfigJson == "" {
+		return nil
+	}
+
+	var proxyDeviceConfig ProxyDeviceConfig
+	err := json.Unmarshal([]byte(proxyDeviceConfigJson), &proxyDeviceConfig)
+	if err != nil {
+		return nil
+	}
+	return &proxyDeviceConfig
+}
+
+type connectCountry struct {
+	LocationId  server.Id
+	Country     string
+	CountryCode string
+}
+
+// county code is lower
+var countryCodeConnectCountries = sync.OnceValue(func() map[string]*connectCountry {
+	ctx := context.Background()
+
+	m := map[string]*connectCountry{}
+
+	server.Db(ctx, func(conn server.PgConn) {
+		result, err := conn.Query(
+			ctx,
+			`
+			SELECT
+				location_id,
+				location_name,
+				country_code
+			FROM location
+			WHERE location_type = $1
+			`,
+			LocationTypeCountry,
+		)
+		server.WithPgResult(result, err, func() {
+			var c connectCountry
+			server.Raise(result.Scan(
+				&c.LocationId,
+				&c.Country,
+				&c.CountryCode,
+			))
+		})
+	})
+
+	return m
+})
+
+func GetConnectLocationForCountryCode(ctx context.Context, countryCode string) *sdk.ConnectLocation {
+	normalCountryCode := strings.ToLower(countryCode)
+	c, ok := countryCodeConnectCountries()[normalCountryCode]
+	if !ok {
+		return nil
+	}
+
+	return &sdk.ConnectLocation{
+		ConnectLocationId: &sdk.ConnectLocationId{
+			LocationId: server.ToSdkId(c.LocationId),
+		},
+		Name:         c.Country,
+		LocationType: sdk.LocationTypeCountry,
+
+		Country:     c.Country,
+		CountryCode: c.CountryCode,
+
+		CityLocationId:    nil,
+		RegionLocationId:  nil,
+		CountryLocationId: server.ToSdkId(c.LocationId),
+	}
+}
