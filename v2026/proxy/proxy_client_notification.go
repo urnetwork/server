919961b6cb94bc4430cb54b9193ac4ca0dda5f60
Product Builder <builder@bringyour.com>
2026-02-20 13:56:18 -0800

    2026.2.20-868244580

---
+++ v2026/proxy/proxy_client_notification.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"golang.org/x/exp/maps"
+
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/glog/v2026"
+	// "github.com/urnetwork/proxy/v2026"
+	"github.com/urnetwork/server/v2026"
+	"github.com/urnetwork/server/v2026/model"
+	// "github.com/urnetwork/server/v2026/router"
+)
+
+type ProxyClientsFunction = func(proxyClients []*model.ProxyClient)
+
+type proxyClientNotification struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	settings *ProxySettings
+
+	proxyClientsCallbacks *connect.CallbackList[ProxyClientsFunction]
+}
+
+func newProxyClientNotification(ctx context.Context, settings *ProxySettings) *proxyClientNotification {
+	cancelCtx, cancel := context.WithCancel(ctx)
+	p := &proxyClientNotification{
+		ctx:                   cancelCtx,
+		cancel:                cancel,
+		settings:              settings,
+		proxyClientsCallbacks: connect.NewCallbackList[ProxyClientsFunction](),
+	}
+	go server.HandleError(p.run, cancel)
+	return p
+}
+
+func (self *proxyClientNotification) run() {
+	defer self.cancel()
+
+	proxyHost := server.RequireHost()
+	block := server.RequireBlock()
+
+	monitor := connect.NewMonitor()
+
+	go server.HandleError(func() {
+		defer self.cancel()
+
+		event, unsub := server.Subscribe(self.ctx, model.ProxyClientChannel(proxyHost, block))
+		defer unsub()
+
+		for {
+			select {
+			case <-self.ctx.Done():
+				return
+			case <-event:
+				glog.Infof("[proxy]notify\n")
+				monitor.NotifyAll()
+			}
+		}
+	})
+
+	nextChangeId := int64(0)
+	for {
+		notify := monitor.NotifyChannel()
+		proxyClients, maxChangeId, err := model.GetProxyClientsSince(
+			self.ctx,
+			proxyHost,
+			block,
+			nextChangeId,
+		)
+		if err != nil {
+			glog.Infof("[proxy]err=%s\n", err)
+		} else if 0 < len(proxyClients) {
+			glog.Infof("[proxy]found %d new proxy clients (%d..%d)\n", len(proxyClients), nextChangeId, maxChangeId)
+			self.proxyClients(maps.Values(proxyClients))
+			nextChangeId = maxChangeId + 1
+		}
+		select {
+		case <-self.ctx.Done():
+			return
+		case <-notify:
+		case <-time.After(self.settings.NotificationTimeout):
+		}
+	}
+}
+
+func (self *proxyClientNotification) proxyClients(proxyClients []*model.ProxyClient) {
+	for _, proxyClientsCallback := range self.proxyClientsCallbacks.Get() {
+		server.HandleError(func() {
+			proxyClientsCallback(proxyClients)
+		})
+	}
+}
+
+func (self *proxyClientNotification) AddProxyClientsCallback(proxyClientsCallback ProxyClientsFunction) func() {
+	callbackId := self.proxyClientsCallbacks.Add(proxyClientsCallback)
+	return func() {
+		self.proxyClientsCallbacks.Remove(callbackId)
+	}
+}
