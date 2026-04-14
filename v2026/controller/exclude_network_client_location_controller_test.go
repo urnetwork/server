3776c3ea4332654a68a4385f24fbdd398cf26b88
Product Builder <builder@bringyour.com>
2026-04-14 14:12:53 -0700

    2026.4.14-914010260

---
+++ v2026/controller/exclude_network_client_location_controller_test.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/urnetwork/server/v2026"
+	"github.com/urnetwork/server/v2026/jwt"
+	"github.com/urnetwork/server/v2026/model"
+	"github.com/urnetwork/server/v2026/session"
+)
+
+func TestNetworkBlocking(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+
+		ctx := context.Background()
+		networkId := server.NewId()
+		clientId := server.NewId()
+
+		userSession := session.Testing_CreateClientSession(ctx, &jwt.ByJwt{
+			NetworkId: networkId,
+			ClientId:  &clientId,
+		})
+
+		location := &model.Location{
+			LocationType: model.LocationTypeCity,
+			City:         "Kamakura",
+			Region:       "Kanagawa",
+			Country:      "Japan",
+			CountryCode:  "jp",
+		}
+		model.CreateLocation(ctx, location)
+
+		/**
+		 * block location
+		 */
+		NetworkBlockLocation(
+			&NetworkBlockLocationArgs{
+				LocationId: location.LocationId,
+			},
+			userSession,
+		)
+
+		result, err := GetNetworkBlockedLocations(userSession)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, len(result.BlockedLocations), 1)
+
+		/**
+		 * unblock location
+		 */
+
+		NetworkUnblockLocation(
+			&NetworkUnblockLocationArgs{
+				LocationId: location.LocationId,
+			},
+			userSession,
+		)
+
+		result, err = GetNetworkBlockedLocations(userSession)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, len(result.BlockedLocations), 0)
+
+	})
+}
