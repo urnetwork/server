5c825fd76bef12af5b39d406c52609ba82aa08dd
Product Builder <builder@bringyour.com>
2026-04-29 10:59:46 -0700

    2026.4.29-926854250

---
+++ v2026/api/handlers/leaderboard_handlers.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/urnetwork/server/v2026/controller"
+	"github.com/urnetwork/server/v2026/router"
+)
+
+func GetLeaderboard(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputNoAuth(controller.GetLeaderboard, w, r)
+}
+
+func GetLeaderboardNetworkRanking(w http.ResponseWriter, r *http.Request) {
+	router.WrapRequireAuth(controller.GetNetworkLeaderboardRanking, w, r)
+}
+
+func SetNetworkLeaderboardPublic(w http.ResponseWriter, r *http.Request) {
+	router.WrapWithInputRequireAuth(controller.SetNetworkLeaderboardRankingPublic, w, r)
+}
