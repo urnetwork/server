0874893d1afaac806ad62d5eea64ce72b16e1c3b
Product Builder <builder@bringyour.com>
2026-05-19 12:43:20 -0700

    2026.5.19-944196410

---
+++ v2026/model/blockchain_model_test.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/urnetwork/server/v2026"
+)
+
+func TestBlockchainParsing(t *testing.T) {
+	server.DefaultTestEnv().Run(func() {
+
+		blockchain := "sol"
+		parsedBlockchain, err := ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, SOL)
+
+		blockchain = "solana"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, SOL)
+
+		blockchain = "matic"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, MATIC)
+
+		blockchain = "poly"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, MATIC)
+
+		blockchain = "polygon"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, MATIC)
+
+		blockchain = "eth"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, ETHEREUM)
+
+		blockchain = "ethereum"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, parsedBlockchain, ETHEREUM)
+
+		blockchain = "invalid_chain"
+		parsedBlockchain, err = ParseBlockchain(blockchain)
+		assert.NotEqual(t, err, nil)
+
+	})
+}
