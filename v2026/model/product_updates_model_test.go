fe1f08f636e4d6b0ea0c49f38e2b7253f58b4663
Product Builder <builder@bringyour.com>
2026-08-27 07:44:22 -0700

    2026.8.27-1030415850

---
+++ v2026/model/product_updates_model_test.go
@@ -0,0 +1,38 @@
+package model
+
+import (
+	"context"
+	"testing"
+
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/server/v2026"
+	// "github.com/urnetwork/server/v2026/jwt"
+	// "github.com/urnetwork/server/v2026/session"
+)
+
+func TestProductUpdates(t *testing.T) {
+	server.DefaultTestEnv().Run(t, func(t testing.TB) {
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		fakeUserId := server.NewId()
+
+		productUpdates, userEmails := GetProductUpdateUserEmailsForUser(ctx, fakeUserId)
+		connect.AssertEqual(t, productUpdates, false)
+		connect.AssertEqual(t, len(userEmails), 0)
+
+		userEmailUserIds := GetUserEmailsForProductUpdatesSync(ctx)
+		connect.AssertEqual(t, len(userEmailUserIds), 0)
+
+		userIdProductUpdatesSync := map[server.Id]bool{}
+		SetProductUpdatesSyncForUsers(ctx, userIdProductUpdatesSync)
+
+		userEmailNetworkIds := GetNetworkUserEmailsForProductUpdatesSync(ctx)
+		connect.AssertEqual(t, len(userEmailNetworkIds), 0)
+
+		networkIdProductUpdatesSync := map[server.Id]bool{}
+		SetNetworkProductUpdatesSyncForUsers(ctx, networkIdProductUpdatesSync)
+
+	})
+}
